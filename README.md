85e24ff62acb767748ea113997ab65f8ac6d55a9
anraku <anraku90@gmail.com>
2019-03-21 21:36:01 +0900

    add eslint

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ## 参考資料
 https://qiita.com/akirakudo/items/77c3cd49e2bf39da79dd
+
+https://www.youtube.com/watch?v=MhkGQAoc7bc
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const App = () => (
+const App = ({ name }) => (
   <div>
-    Hello すごいWorld!!
+    Hello {name}
   </div>
 )
 
